7287a459585a7e1acebb5fe56340b0f2d5ed3cbe
Matt Hampson <mhampson@mail.immaculata.edu>
2023-03-19 16:20:17 -0400

    this is rough

---
+++ main.py
@@ -0,0 +1,22 @@
+"""
+Week 8
+This is to test how many hours a grocery store is open a week
+"""
+
+class Grocery_Store:
+    """
+    bla
+    """
+    def __init__(self, hours, days):
+        self.hours = hours
+        self.days = days
+
+def total_hours(self):
+    """
+    This calculates weekly total hours of operation
+    """
+    weekly_hours = self.hours * self.days
+    return weekly_hours
+
+if __name__ == "__main__":
+    ten_hours = Grocery_Store(10, 7)
---
+++ test_main.py
@@ -0,0 +1,8 @@
+"""
+This is the test for how many hours a week a grocery store is open
+"""
+
+from main import Grocery_Store
+
+def test_for_weekly_hours():
+    assert (10, 7) == 70
